3f3bacc05bbc7996790f61d87d5467b50306e505
Royce <zapp917@yahoo.com>
2019-05-21 21:27:40 -0700

    solved assignment 4

--- cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
+++ cmp-databinding-assignment-start/src/app/game-control/game-control.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
@@ -6,20 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-control.component.css']
 })
 export class GameControlComponent implements OnInit {
+  @Output() intervalFired = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
 
   constructor() { }
 
   ngOnInit() {
   }
 
-  startGame() {
-    setInterval(function () {
-      let i = 0;
+  onStartGame() {
+    this.interval = setInterval(() => {
+      this.intervalFired.emit(this.lastNumber + 1);
+      this.lastNumber++;
     }, 1000);
   }
 
-  endGame() {
-
+  onPauseGame() {
+    clearInterval(this.interval);
   }
 
 }
