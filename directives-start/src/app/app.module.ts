38ccc91f7be6b5a8b913004302d252b98a73ca7f
Royce <zapp917@yahoo.com>
2019-05-22 22:31:09 -0700

    updated Section 5 application lecture 92

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BasicHighlightDirective } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective
   ],
   imports: [
     BrowserModule,
